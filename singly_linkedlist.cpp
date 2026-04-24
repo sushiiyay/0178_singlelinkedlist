6badb91078c962fa8adcc63fad9b90a1205805fc
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-24 15:37:03 +0700

    Menambahkan bool

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -44,7 +44,18 @@ public:
 
         while ((current != NULL) && (nim >= current->noMhs))
         {
-            if
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
         }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
+
+    bool listEmpty()
 }
