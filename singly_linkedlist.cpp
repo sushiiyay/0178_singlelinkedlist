3e3afc1af5c745cc6bffbba3917acb34be567f99
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-24 15:44:20 +0700

    Menambahkan bool delNode

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -58,4 +58,23 @@ public:
     }
 
     bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+    }
+
+    booldelNode
 }
