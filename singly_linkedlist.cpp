8a1e74b9dea0a517cd6ab1e88a949768320213e0
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-24 15:50:34 +0700

    Menambahkan void traverse

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -76,5 +76,20 @@ public:
         return (*current != NULL);
     }
 
-    booldelNode
+    booldelNode(int nim)
+    {
+        Node *current, *previous;
+        if (Search(nim, &previous, &current))
+        return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+    
+    void traverse()
 }
